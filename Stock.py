1eb92d528a852aec62b4909260a66ed90a3db922
VIshalwani1899 <66123796+VIshalwani1899@users.noreply.github.com>
2024-04-13 20:26:22 +0530

    Update Stock.py
    for hit counts

--- Stock.py
+++ Stock.py
@@ -3,6 +3,76 @@ import yfinance as yf  # Import yfinance directly in main.py
 from prophet import Prophet
 import pandas as pd
 import matplotlib.pyplot as plt  # Import matplotlib for visualization
+import requests
+import base64
+
+# GitHub repository details
+GITHUB_USERNAME = "VIshalwani1899"
+GITHUB_REPO_NAME = "Model"
+GITHUB_TOKEN = "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"
+HIT_COUNT_FILE_PATH = "hit_count.txt"
+
+GITHUB_API_URL = "https://api.github.com/repos/VIshalwani1899/Stocks/contents/hit_count.txt"
+
+def increment_hit_count():
+    """
+    Increments the hit count by updating the file on GitHub.
+    """
+    # Fetch current hit count from GitHub
+    hit_count = get_hit_count_from_github()
+
+    # Increment hit count
+    hit_count += 1
+
+    # Update hit count on GitHub
+    update_hit_count_on_github(hit_count)
+
+    return hit_count
+
+def get_hit_count_from_github():
+    # Retrieve the current hit count from the hit count file on GitHub
+    url = f"https://raw.githubusercontent.com/VIshalwani1899/Stocks/main/hit_count.txt"
+    print("url", url)
+    response = requests.get(url)
+    if response.status_code == 200:
+        print("scueess")
+        print(response.text.strip())
+        return int(response.text.strip())
+    else:
+        print("fail")
+        # If file does not exist, return 0
+        return 0
+
+def update_hit_count_on_github(hit_count):
+    """
+    Updates the hit count file on GitHub with the new hit count.
+    """
+    # Convert hit count to string and encode it in Base64 format
+    content_bytes = f"{hit_count}".encode('utf-8')
+    content_base64 = base64.b64encode(content_bytes).decode('utf-8')
+
+    headers = {
+        "Authorization": f"token {GITHUB_TOKEN}",
+        "Accept": "application/vnd.github.v3+json"
+    }
+    data = {
+        "message": "Update hit count",
+        "content": content_base64,
+        "sha": get_file_sha()
+    }
+    response = requests.put(GITHUB_API_URL, headers=headers, json=data)
+    if response.status_code == 200:
+        print("Hit count updated successfully!")
+    else:
+        print("Failed to update hit count:", response.text)
+
+def get_file_sha():
+    response = requests.get(GITHUB_API_URL, headers={"Authorization": f"token {GITHUB_TOKEN}"})
+    if response.status_code == 200:
+        return response.json()["sha"]
+    else:
+        return None
+
 
 def get_stock_data(stock_ticker):
     """
@@ -61,8 +131,10 @@ def visualize_forecast_components(m, forecast, current_price, forecast_days):
     st.write(f"Date of Forecasted Price: {forecast_date}")
 
 def main():
+    # State variable to track button click status (default `False`)
+    #clicked = False
+    hit_count = increment_hit_count()
     # Create Streamlit header with CSS styling
-    
     st.markdown("""
        <style>
            .header {
@@ -104,8 +176,8 @@ def main():
         #vishal_link = "[Vishal Wani](https://www.linkedin.com/in/vishal-wani-b006111b0/)"  # Replace with Vishal's LinkedIn URL
         #st.write(vishal_link, unsafe_allow_html=True)
     st.write("---")
-
-    st.write("Developed by: **[Sachin Shinkar](https://www.linkedin.com/in/sachinshinkar/)** & **[Vishal Wani](https://www.linkedin.com/in/vishal-wani-b006111b0/)**")
+    st.write("Total Hits: ", hit_count)
+    st.write("Developed by: **[Sachin Shinkar](https://www.linkedin.com/in/sachinshinkar/)** and **[Vishal Wani](https://www.linkedin.com/in/vishal-wani-b006111b0/)**")
 
 if __name__ == "__main__":
     main()
